ec25a296c990abac687c517d216d6b555f43ddb0
Ariyike Adetimehin <ariyike.aa@gmail.com>
2018-10-09 15:03:48 +0100

    more volcano edits, map now added
    added map, cluster feature non functional
    link to volcano data non functional

--- hoppler/pages/volcanoMap.js
+++ hoppler/pages/volcanoMap.js
@@ -1,5 +1,4 @@
-//<script>
-        var map;
+var map;
         //initMap() called when Google Maps API code is loaded - when web page is opened/refreshed 
         function initMap() {
             map = new google.maps.Map(document.getElementById('map'), {
@@ -13,24 +12,26 @@
         var titleName;
         $(document).ready(function () {
 
-            $('#earthquakes').click(function () {
+            $('#volcanoes').click(function () {
                 // Set Google map  to its start state
                 map = new google.maps.Map(document.getElementById('map'), {
                     zoom: 2,
                     center: new google.maps.LatLng(2.8, -187.3), // Center Map. Set this to any location that you like
                     mapTypeId: 'terrain' // can be any valid type
                 });
+                // The following uses JQuery library
                 $.ajax({
                     // The URL of the specific data required
-                    url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson",
+                    url: "https://webservices.volcano.si.edu/geoserver/GVP-VOTW/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=GVP-VOTW:Smithsonian_VOTW_Holocene_Volcanoes&maxFeatures=100&outputFormat=application/json",
 
                     // Called if there is a problem loading the data
                     error: function () {
                         $('#info').html('<p>An error has occurred</p>');
                     },
-
-                    // The data has succesfully loaded
+                    // Called when the data has succesfully loaded
                     success: function (data) {
+                        i = 0;
+                        var markers = []; // keep an array of Google Maps markers, to be used by the Google Maps clusterer
                         $.each(data.features, function (key, val) {
                             // Get the lat and lng data for use in the markers
                             var coords = val.geometry.coordinates;
@@ -38,24 +39,23 @@
                             // Now create a new marker on the map
                             var marker = new google.maps.Marker({
                                 position: latLng,
-                                map: map,
-                                label: val.properties.mag.toString() // Whatever label you like. This one is the magnitude of the earthquake
+                                map: map
                             });
+                            markers[i++] = marker; // Add the marker to array to be used by clusterer
+                            //------
+                             marker.addListener('click', function (data) {
+                                infowindow.open(map, marker); // Open the Google maps marker infoWindow
+                            });
+                            
                             // Form a string that holds desired marker infoWindow content. The infoWindow will pop up when you click on a marker on the map
                             var infowindow = new google.maps.InfoWindow({
                                 content: "<h3>" + val.properties.title + "</h3><p><a href='" + val.properties.url + "'>Details</a></p>"
                             });
-                            marker.addListener('click', function (data) {
-                                infowindow.open(map, marker); // Open the Google maps marker infoWindow
-                            });
+                            //-----
                         });
-
+                        var markerCluster = new MarkerClusterer(map, markers,
+                            { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
                     }
                 });
             });
         });
-//    </script>
-
-    <!-- Need the following code for Google Maps. PLEASE INSERT YOUR OWN GOOGLE MAPS KEY BELOW -->
-//    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDUjRBa2C5gS3B3lId-9m-FsoBFwJDB3UQ&callback=initMap">
-//    </script>
